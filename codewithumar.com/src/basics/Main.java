25270a2982200f6c3d7f66bd6fa4a0dc42a2414b
umar.alam <umar.alam@WBLL0817E3761.onmobile.com>
2021-02-25 03:25:18 +0530

    main committed

--- codewithumar.com/src/basics/Main.java
+++ codewithumar.com/src/basics/Main.java
@@ -3,13 +3,20 @@ package basics;
 public class Main {
 	public static void main(String args[]) {
 		//1234
-		int number = 1234;
+		int number = 12321;
+		int temp = number;
 		int rev = 0;
 		while(number > 0) {
 			rev = rev * 10 + number % 10;
 			number = number / 10;
 		}
 		System.out.println(rev);
+		if (rev ==  temp) {
+			System.out.println("number is palindrom");
+		}
+			else {
+				System.out.println("number is not palindrome");
+			}
 	}
 
 }
